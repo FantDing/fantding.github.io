a5441c5d1a5edfe2687d3f3a254f76f4ce0bc6f7
dingwei14 <dingwei14@meituan.com>
2022-06-11 15:41:16 +0800

    Site updated: 2022-06-11 15:41:16

---
+++ js/disqusRemoveAd.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     setInterval(() => {
+//         var items=$("#disqus_thread iframe");
+//         len=items.length
+//         for (let index = 0; index < len; index++) {
+//             if(index!=1){
+//                 var element = items[index];
+//                 console.log(items.length)
+//                 element.remove()
+//             }
+//         }
+//     }, 300);
+// })
+
